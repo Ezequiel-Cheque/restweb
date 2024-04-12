2b841f8b65b1736daccefd38cced62b0b941b042
Ezequiel-cheque <delaluzezequiel@gmail.com>
2024-04-11 18:54:19 -0600

    FEAT: start webserver

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
+import fs from 'fs';
 import http from 'http';
 
 const server = http.createServer((req, res) => {
 
-    console.log(req.url);
+    // console.log(req.url);
 
-    const data = { name: "Jhon Doe", age: 30, city: 'New York' };
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(data));
+    // const data = { name: "Jhon Doe", age: 30, city: 'New York' };
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
+    // res.end(JSON.stringify(data));
+
+    if ( req.url === "/" ) {
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(htmlFile);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end();
+    }
 
 });
 
